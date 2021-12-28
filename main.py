6c48d595da2093da493800fd39379c4d34c98320
Phil Lundeen <phillundeen@rivian.com>
2021-12-28 13:42:54 -0800

    Adds a /table endpoint to demo html reponse

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import multiprocessing
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -16,6 +17,32 @@ def root():
 def test():
     """test endpoint"""
     return "Yep, this is the test endpoint all right!"
+
+@app.get("/table")
+def table():
+    """return an HTML table"""
+    data = '''
+    <html>
+        <body>
+            <table>
+                <thead>
+                    <tr>
+                    <td>Foo</td>
+                    <td>Bar</td>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                </tr>
+            </table>
+        </body>
+    </html>
+    '''
+
+    # with open('C:/Users/phillundeen/AppData/Local/Temp/failure_table.html', 'r') as f:
+    #     data = f.read()
+    return HTMLResponse(content=data, status_code=200)
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
