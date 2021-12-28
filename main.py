e31179ec6fd5df891fb68e75aa6163a9b2d56ad1
Phil Lundeen <phillundeen@rivian.com>
2021-12-28 13:57:36 -0800

    Adds demo for mounting static files

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import multiprocessing
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def root():
